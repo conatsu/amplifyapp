f2c60fb24e6756741c8c4d56dc91f84bf804cd5a
conatsu <conachan1125@gmail.com>
2022-07-23 17:55:33 +0900

    add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "amplifytest": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -27,7 +27,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://h3xmmiqs5rdm5l6rrfd2x2jiee.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-v2hxbgi2gnh37ikflssxbzj5aq"
+    "aws_appsync_apiKey": "da2-v2hxbgi2gnh37ikflssxbzj5aq",
+    "aws_user_files_s3_bucket": "amplifytestbucket150055-dev",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
 
 
